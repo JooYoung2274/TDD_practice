9472546f8a7d112c76b5de46020f153e9b16efa6
JooYoung2274 <jooooo2274@gmail.com>
2022-10-05 08:59:00 +0900

    [refac] service layer test code (mock 함수 제거)

---
+++ test/unit/products.service.test.ts
@@ -0,0 +1,38 @@
+import { ProductsService } from '../../service/products.service';
+import newProduct from '../data/new-product.json';
+import updateProductJson from '../data/update-product.json';
+
+describe('Products Service layer unit test', () => {
+    const productsRepository = {
+        createProduct: async () => {
+            return newProduct;
+        },
+
+        getProduct: async () => {
+            return newProduct;
+        },
+
+        updateProduct: async () => {
+            return updateProductJson;
+        },
+    };
+
+    const name = 'kim';
+
+    // @ts-ignore
+    const productsService = new ProductsService(productsRepository);
+    it('createProduct', async () => {
+        const result = await productsService.createProduct(newProduct);
+        expect(result).toBe(newProduct);
+    });
+
+    it('getProduct', async () => {
+        const result = await productsService.getProduct(name);
+        expect(result).toBe(newProduct);
+    });
+
+    it('updateProduct', async () => {
+        const result = await productsService.updateProduct(name);
+        expect(result).toBe(updateProductJson);
+    });
+});
