f01db3e3397e9337b7a3c6e6c108a3a197b3c8d3
JooYoung2274 <jooooo2274@gmail.com>
2023-02-02 18:40:53 +0900

    prac test code 수정

--- src/test/unit/products.service.test.ts
+++ src/test/unit/products.service.test.ts
@@ -1,48 +1,48 @@
 import ProductsService from '../../products/products.service';
 
 describe('products Service layer unit test', () => {
-    const newProduct = {
-        name: 'kim',
-        description: 'good',
-        price: 15,
-    };
+  const newProduct = {
+    name: 'kim',
+    description: 'good',
+    price: 15,
+  };
 
-    const updateProductJson = {
-        name: 'test',
-        description: 'good',
-        price: 15,
-    };
+  const updateProductJson = {
+    name: 'test',
+    description: 'good',
+    price: 15,
+  };
 
-    const productsRepository = {
-        createProduct: async () => {
-            return newProduct;
-        },
+  const productsRepository = {
+    createProduct: async () => {
+      return newProduct;
+    },
 
-        getProduct: async () => {
-            return newProduct;
-        },
+    getProduct: async () => {
+      return newProduct;
+    },
 
-        updateProduct: async () => {
-            return updateProductJson;
-        },
-    };
+    updateProduct: async () => {
+      return updateProductJson;
+    },
+  };
 
-    const name = 'kim';
+  const name = 'kim';
 
-    // @ts-ignore
-    const productsService = new ProductsService(productsRepository);
-    it('createProduct', async () => {
-        const result = await productsService.createProduct(newProduct);
-        expect(result).toBe(newProduct);
-    });
+  // @ts-ignore
+  const productsService = new ProductsService(productsRepository);
+  it('createProduct', async () => {
+    const result = await productsService.createProduct(newProduct);
+    expect(result).toBe(newProduct);
+  });
 
-    it('getProduct', async () => {
-        const result = await productsService.getProduct(name);
-        expect(result).toBe(newProduct);
-    });
+  it('getProduct', async () => {
+    const result = await productsService.getProduct(name);
+    expect(result).toBe(newProduct);
+  });
 
-    it('updateProduct', async () => {
-        const result = await productsService.updateProduct(name);
-        expect(result).toBe(updateProductJson);
-    });
+  it('updateProduct', async () => {
+    const result = await productsService.updateProduct(name);
+    expect(result).toBe(updateProductJson);
+  });
 });
