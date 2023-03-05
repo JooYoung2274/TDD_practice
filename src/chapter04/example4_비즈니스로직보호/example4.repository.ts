caf100dd86ebd5c5e4b2fe0ccc38fe9cf3e4c654
JooYoung2274 <jooooo2274@gmail.com>
2023-03-06 08:58:36 +0900

    [update] readme

--- src/chapter04/example4_비즈니스로직보호/example4.repository.ts
+++ src/chapter04/example4_비즈니스로직보호/example4.repository.ts
@@ -2,8 +2,9 @@ import { Producer } from '../../models/Producer';
 import { IExample4Repository } from './example4.interface';
 
 export class Example4Repository implements IExample4Repository {
-  async saveShortfall(shortfall: number): Promise<void> {
+  async saveShortfall(shortfall: number): Promise<number> {
     await Producer.create();
+    return 1;
   }
   async saveProfit(profit: number): Promise<void> {
     await Producer.create();
